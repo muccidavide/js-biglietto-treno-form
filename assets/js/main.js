eb0b454dd07a9352c7062ffdba3c3602317dc0f8
muccidavide <davidemucci0804@gmail.com>
2022-03-25 15:44:00 +0100

    Add random values

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,14 @@ function myFunction() {
     
     const full_name = document.getElementById("full_nameid").value; // raccolta dato nome
     document.getElementById("passenger_name").innerHTML = full_name;
-    console.log(full_name); 
 
     const km_trip = document.getElementById("km_tripid").value; // raccolta dato km
-    console.log(km_trip);
 
     const age = document.getElementById("age_id").value; // raccolta dato eta
     document.getElementById('discount_type').innerHTML = age;
-    console.log(age);
 
     const cost_for_km = 0.21;
-    const total_price = km_trip * cost_for_km; // Prezzo Totale
-    console.log(total_price); 
+    const total_price = km_trip * cost_for_km; // Prezzo Totale 
     
     const under_18_discount = (total_price * 0.8).toFixed(2);
     const over_65_discount = (total_price *0.6).toFixed(2);
@@ -25,17 +21,20 @@ function myFunction() {
 
     if (age == "Under 18") {
         price = (under_18_discount);
-        console.log(price);
-        document.getElementById('final_price').innerHTML = price;
+        document.getElementById('final_price').innerHTML = price + "€";
     } else if(age == "Over 65" ){
         price = (over_65_discount);
-        console.log(price);
-        document.getElementById('final_price').innerHTML = price;
+        document.getElementById('final_price').innerHTML = price + "€";
     } else {
         price = total_price.toFixed(2);
-        console.log(price);
-        document.getElementById('final_price').innerHTML = price;
+        document.getElementById('final_price').innerHTML = price + "€";
     }
+
+    const carriage = (Math.random()*10).toFixed(0)
+    document.getElementById('carriage_number').innerHTML = carriage;
+
+    const cd_code = (Math.random()*100000).toFixed(0)
+    document.getElementById('code_number').innerHTML = cd_code;
 
     
 }
